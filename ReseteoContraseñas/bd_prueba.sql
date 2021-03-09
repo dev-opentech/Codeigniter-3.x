bd1a8018b29e65a5a755e05bf0ec38325ed6fac2
Open Technology Dev Team <79714365+dev-opentech@users.noreply.github.com>
2021-03-09 18:16:36 -0300

    Update bd_prueba.sql

--- ReseteoContraseñas/bd_prueba.sql
+++ ReseteoContraseñas/bd_prueba.sql
@@ -1,1 +1,16 @@
-asdfasdf
+create table usuario(
+id_usuario int not null primary key auto_increment,
+nombre_usuario varchar(200),
+contrasenia varchar(200),
+rela_persona int,
+rela_tipo_usuario int,
+url_update varchar(100),
+time_update_pass datetime,
+);
+
+create table persona(
+id_persona int not null primary key auto_increment,
+nombre_persona varchar(100),
+apellido_persona varchar(100),
+email_persona varchar(100),
+);
